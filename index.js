cd5186234e1dd714048073c933b266108318bb20
Vishal Jaiswar <vishaldevloperfd@gmail.com>
2024-07-24 13:28:59 +0530

    updates

---
+++ index.js
@@ -0,0 +1,12 @@
+// let a = 10;
+// let b = 12;
+// let c = a+b;
+// console.log(c);
+
+// for(var i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+// for(var i = 10; i >= 0; i--){
+//     console.log(i)
+// }
