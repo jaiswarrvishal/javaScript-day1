37335e67d20fb1e2310bccf8923b4596467fbf49
Vishal Jaiswar <vishaldevloperfd@gmail.com>
2024-07-24 22:23:05 +0530

    updates

--- index.js
+++ index.js
@@ -10,3 +10,35 @@
 // for(var i = 10; i >= 0; i--){
 //     console.log(i)
 // }
+
+/*
+Q1. Sum of all the Natural Numbers from 1 to n
+-> sum of 1 to 5: 1+2+3+4+5=>15
+*/
+// function sumOfNaturalNumber(num){
+//     let sum = 0;
+//     for(let i = 1; i <= num; i++){
+//         sum = sum + i;
+//     }
+//     return sum;
+    
+//     // return num * (num + 1)/2;
+// }
+// console.log(sumOfNaturalNumber(5));
+
+
+/*
+Q2. Sum of digits of a numbers
+-> 1287 => 1+2+8+7=18
+-> 1287%10: remainder=128, Q=7 => 128%10: r=12, q=8 =>12%10: r=2, q=1
+Solution:
+*/
+function sumOfDigits(num){
+    let sum = 0;
+    while(num > 0){
+        sum += num%10;
+        num += Math.floor(num/10)
+    }
+    return sum;
+}
+console.log(sumOfDigits(1287));
